d16568c03f45156425d2a70cd92d5b9f973e9f59
Ken Chan <ken.chan@swivelsoftware.com>
2018-08-24 17:00:41 +0800

    Fix Bug

--- workflow/event-definitions/event-handlers/checkETAChange.js
+++ workflow/event-definitions/event-handlers/checkETAChange.js
@@ -2,13 +2,15 @@
 function CheckETA() {
 	this.returns = ["DATESET", "DELAY"];
 	this.handle = function(definition, data, handlerParameters, helper) {
+		console.log('Check ETA Changed')
 		if (data.data.estimatedArrivalDate) {
 			if(data.oldData.estimatedArrivalDate == null) {
-				console.log("NEW DEPARTURE DATE FOR BILL %s: %s", data.data.id, data.data.estimatedArrivalDate);
+				console.log("NEW Arrival DATE FOR BILL %s: %s", data.data.id, data.data.estimatedArrivalDate);
 				helper.persistence.models.bill.findOne({ id: data.data.id })
 					.then((bill) => {
+						console.log('FM3000 ETA', bill.estimatedArrivalDate)
 						if (bill.estimatedArrivalDate > data.data.estimatedArrivalDate) {
-							console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedDepartureDate);
+							console.log("ETA DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedDepartureDate);
 							return "DELAY";
 						} else {
 							return "DATESET";
--- workflow/event-definitions/event-handlers/checkETDChange.js
+++ workflow/event-definitions/event-handlers/checkETDChange.js
@@ -2,11 +2,13 @@
 function CheckETD() {
 	this.returns = ["DATESET", "DELAY","CHANGED"];
 	this.handle = function(definition, data, handlerParameters, helper) {
+		console.log('Check ETD Changed')
 		if(data.data.estimatedDepartureDate) {
 			if(data.oldData.estimatedDepartureDate == null) {
 				console.log("NEW DEPARTURE DATE FOR BILL %s: %s", data.data.id, data.data.estimatedDepartureDate);
 				helper.persistence.models.bill.findOne({ id: data.data.id })
 					.then((bill) => {
+						console.log('FM3000 ETD', bill.estimatedDepartureDate)
 						if (bill.estimatedDepartureDate < data.data.estimatedDepartureDate) {
 							console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedDepartureDate);
 							return "DELAY";
