91a96ee758ab14366603c829219c312085b38d1b
Ken Chan <ken.chan@swivelsoftware.com>
2018-08-27 15:19:23 +0800

    Fix ETD Bug

--- workflow/event-definitions/event-handlers/checkETDChange.js
+++ workflow/event-definitions/event-handlers/checkETDChange.js
@@ -3,22 +3,24 @@ function CheckETD() {
 	this.returns = ["DATESET", "DELAY","CHANGED"];
 	this.handle = function(definition, data, handlerParameters, helper) {
 		console.log('Check ETD Changed')
-		if(data.data.estimatedDepartureDate) {
-			if(data.oldData.estimatedDepartureDate == null) {
-				console.log("NEW DEPARTURE DATE FOR BILL %s: %s", data.data.id, data.data.estimatedDepartureDate);
+		console.log('Old ETD', data.oldData.lastStatuseDetails.estimatedDepartureDate)
+		console.log('New ETD', data.data.lastStatuseDetails.estimatedDepartureDate)
+		if (data.data.lastStatuseDetails.estimatedDepartureDate) {
+			if (data.oldData.lastStatuseDetails.estimatedDepartureDate == null) {
+				console.log("NEW DEPARTURE DATE FOR BILL %s: %s", data.data.id, data.data.lastStatuseDetails.estimatedDepartureDate);
 				helper.persistence.models.bill.findOne({ id: data.data.id })
 					.then((bill) => {
 						console.log('FM3000 ETD', bill.estimatedDepartureDate)
-						if (bill.estimatedDepartureDate < data.data.estimatedDepartureDate) {
-							console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedDepartureDate);
+						if (bill.estimatedDepartureDate < data.data.lastStatuseDetails.estimatedDepartureDate) {
+							console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.lastStatuseDetails.estimatedDepartureDate);
 							return "DELAY";
 						} else {
 							return "DATESET";
 						}
 					})
 			} else {
-				if (data.data.estimatedDepartureDate < data.oldData.estimatedDepartureDate) {
-					console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedDepartureDate);
+				if (data.data.lastStatuseDetails.estimatedDepartureDate < data.oldData.lastStatuseDetails.estimatedDepartureDate) {
+					console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.lastStatuseDetails.estimatedDepartureDate);
 					return "DELAY";
 				}
 			}
