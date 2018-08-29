c506696a327009d5e88b1c1bc21307435e13845e
Ken Chan <ken.chan@swivelsoftware.com>
2018-08-29 09:28:41 +0800

    test use moment to format

--- workflow/event-definitions/event-handlers/checkETDChange.js
+++ workflow/event-definitions/event-handlers/checkETDChange.js
@@ -3,14 +3,15 @@ function CheckETD() {
 	this.returns = ["DATESET", "DELAY","CHANGED"];
 	this.handle = function(definition, data, handlerParameters, helper) {
 		console.log('Check ETD Changed')
-		console.log('Old ETD', data.oldData.lastStatusDetails.estimatedDepartureDate)
-		console.log('New ETD', data.data.lastStatusDetails.estimatedDepartureDate)
+		console.log('Old ETD', data.oldData.lastStatusDetails.estimatedDepartureDate, helper.moment(data.oldData.lastStatusDetails.estimatedDepartureDate).format('YYYY-MM-DD HH:ii:ss'))
+		console.log('New ETD', data.data.lastStatusDetails.estimatedDepartureDate, helper.moment(data.data.lastStatusDetails.estimatedDepartureDate).format('YYYY-MM-DD HH:ii:ss'))
 		if (data.data.lastStatusDetails.estimatedDepartureDate) {
 			if (data.oldData.lastStatusDetails.estimatedDepartureDate == null) {
 				console.log("NEW DEPARTURE DATE FOR BILL %s: %s", data.data.id, data.data.lastStatusDetails.estimatedDepartureDate);
 				helper.persistence.models.bill.findOne({ id: data.data.id })
 					.then((bill) => {
 						console.log('FM3000 ETD', bill.estimatedDepartureDate)
+						data.oldData.bill.estimatedDepartureDate = bill.estimatedDepartureDate;
 						if (bill.estimatedDepartureDate < data.data.lastStatusDetails.estimatedDepartureDate) {
 							console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.lastStatusDetails.estimatedDepartureDate);
 							return "DELAY";
