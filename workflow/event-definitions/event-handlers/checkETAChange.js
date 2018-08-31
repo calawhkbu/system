493ca5314161a44b1d7be90013434ea415a3274a
Ken Chan <ken.chan@swivelsoftware.com>
2018-08-31 10:26:01 +0800

    fix ETD ETA bug

--- workflow/event-definitions/event-handlers/checkETAChange.js
+++ workflow/event-definitions/event-handlers/checkETAChange.js
@@ -2,15 +2,15 @@
 function CheckETA() {
 	this.returns = ["DATESET", "DELAY"];
 	this.handle = function(definition, data, handlerParameters, helper) {
-		console.log('Check ETA Changed')
+		console.log(`[Event Triggered] Check ETA Change for master ${data.masterNo}`)
 		if (data.data.estimatedArrivalDate) {
-			if(data.oldData.estimatedArrivalDate == null) {
-				console.log("NEW Arrival DATE FOR BILL %s: %s", data.data.id, data.data.estimatedArrivalDate);
-				helper.persistence.models.bill.findOne({ id: data.data.id })
+			console.log(`New ETA from SwivelTrack: ${data.data.estimatedArrivalDate}`)
+			if (data.oldData.estimatedArrivalDate == null) {
+				helper.persistence.models.bill.findOne({ customer: data.data.customerId, masterNo: data.data.masterNo })
 					.then((bill) => {
-						console.log('FM3000 ETA', bill.estimatedArrivalDate)
+						console.log(`FM3000 ETA: ${bill.estimatedArrivalDate}`)
 						if (bill.estimatedArrivalDate > data.data.estimatedArrivalDate) {
-							console.log("ETA DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedDepartureDate);
+							console.log(`ETA DELAY FOR BILL ${data.data.masterNo} CUSTOMERID ${data.data.customerId}: ${data.data.estimatedDepartureDate}`);
 							return "DELAY";
 						} else {
 							return "DATESET";
@@ -18,10 +18,14 @@ function CheckETA() {
 					})
 			} else {
 				if (data.data.estimatedArrivalDate > data.oldData.estimatedArrivalDate) {
-					console.log("DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.estimatedArrivalDate);
+					console.log(`ETA DELAY FOR BILL ${data.data.masterNo} CUSTOMERID ${data.data.customerId}: ${data.data.estimatedDepartureDate}`);
 					return "DELAY";
+				} else {
+					return "DATESET";
 				}
 			}
+		} else {
+			return "DATESET";
 		}
 	}
 }
--- workflow/event-definitions/event-handlers/checkETDChange.js
+++ workflow/event-definitions/event-handlers/checkETDChange.js
@@ -2,21 +2,17 @@
 function CheckETD() {
 	this.returns = ["DATESET", "DELAY","CHANGED"];
 	this.handle = function(definition, data, handlerParameters, helper) {
-		console.log('Check ETD Changed')
-		if (data.oldData.lastStatusDetails.estimatedDepartureDate) {
-			console.log('Old ETD', data.oldData.lastStatusDetails.estimatedDepartureDate)
-			console.log('Old ETD in moment', (helper.moment(data.oldData.lastStatusDetails.estimatedDepartureDate)).format('YYYY-MM-DD HH:ii:ss'))
-		}
+		console.log(`[Event Triggered] Check ETA Change for master ${data.masterNo}`)
 		if (data.data.lastStatusDetails.estimatedDepartureDate) {
-			console.log('New ETD', data.data.lastStatusDetails.estimatedDepartureDate)
-			console.log('New ETD in moment', (helper.moment(data.data.lastStatusDetails.estimatedDepartureDate)).format('YYYY-MM-DD HH:ii:ss'))
+			console.log(`New ETD from SwivelTrack: ${data.data.lastStatusDetails.estimatedDepartureDate}`)
+			console.log('New ETD from SwivelTrack', (helper.moment(data.data.lastStatusDetails.estimatedDepartureDate)).format('YYYY-MM-DD HH:ii:ss'))
 			if (data.oldData.lastStatusDetails.estimatedDepartureDate == null) {
 				console.log("NEW DEPARTURE DATE FOR BILL %s: %s", data.data.id, data.data.lastStatusDetails.estimatedDepartureDate);
-				helper.persistence.models.bill.findOne({ id: data.data.id })
+				helper.persistence.models.bill.findOne({ customer: data.data.customerId, masterNo: data.data.masterNo })
 					.then((bill) => {
-						console.log('FM3000 ETD', bill.estimatedDepartureDate)
+						console.log(`FM3000 ETA: ${bill.estimatedArrivalDate}`)
 						if (bill.estimatedDepartureDate < data.data.lastStatusDetails.estimatedDepartureDate) {
-							console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.lastStatusDetails.estimatedDepartureDate);
+							console.log(`ETD DELAY FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}: ${data.data.estimatedDepartureDate}`);
 							return "DELAY";
 						} else {
 							return "DATESET";
@@ -24,10 +20,14 @@ function CheckETD() {
 					})
 			} else {
 				if (data.data.lastStatusDetails.estimatedDepartureDate < data.oldData.lastStatusDetails.estimatedDepartureDate) {
-					console.log("ETD DELAY FOR BILL %s CUSTOMERID %s: %s", data.data.masterNo, data.data.customerId, data.data.lastStatusDetails.estimatedDepartureDate);
+					console.log(`ETD DELAY FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}: ${data.data.estimatedDepartureDate}`);
 					return "DELAY";
+				} else {
+					return "DATESET";
 				}
 			}
+		} else {
+			return "DATESET";
 		}
 	}
 }
