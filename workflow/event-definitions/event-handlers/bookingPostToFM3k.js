f35f46a7a03a1d15c2b926cda344e12acd289a8c
Ken Chan <ken.chan@swivelsoftware.com>
2018-09-17 14:24:51 +0800

    stop send booking

--- workflow/event-definitions/event-handlers/bookingPostToFM3k.js
+++ workflow/event-definitions/event-handlers/bookingPostToFM3k.js
@@ -1,7 +1,7 @@
 function bookingPostToFM3k() {
 
 	this.handle = function(definition, data, handlerParameters, helper) {
-		helper.postBookingToFM3k(data);
+		// helper.postBookingToFM3k(data);
 	}
 }
 
