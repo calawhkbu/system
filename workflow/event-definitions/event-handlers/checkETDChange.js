492562077ebc18a465e9790f62e9c11833689b8f
Ken Chan <ken.chan@swivelsoftware.com>
2018-10-15 11:12:38 +0800

    fix bug in ETD

--- workflow/event-definitions/event-handlers/checkETDChange.js
+++ workflow/event-definitions/event-handlers/checkETDChange.js
@@ -4,14 +4,14 @@ function CheckETD() {
 	this.handle = function(definition, data, handlerParameters, helper) {
 		console.log(`[Event Triggered] Check ETD Change for master ${data.data.masterNo}`)
 		if (data.data.estimatedDepartureDate) {
-			if (data.oldData.estimatedArrivalDate == null) {
+			if (data.oldData.estimatedDepartureDate == null) {
 				var promise = new Promise(function (resolve) {
 					helper.persistence.models.bill.findOne({ where:{ customerId: data.data.customerId, masterNo: data.data.masterNo } })
 						.then((bill) => {
 							var oldEstimatedDepartureDate = helper.moment(bill.estimatedDepartureDate);
 							var newEstimatedDepartureDate = helper.moment(data.data.estimatedDepartureDate);
 							console.log(`[Master NO:${data.data.masterNo}] Compare with FM3000`);
-							console.log(`[Master NO:${data.data.masterNo}] OLD ETD: ${oldEstimatedDepartureDate}, OLD DATA ETA: ${helper.moment(data.oldData.estimatedArrivalDate)}`)
+							console.log(`[Master NO:${data.data.masterNo}] OLD ETD: ${oldEstimatedDepartureDate}, OLD DATA ETD: ${helper.moment(data.oldData.estimatedDepartureDate)}`)
 							console.log(`[Master NO:${data.data.masterNo}] NEW ETD: ${newEstimatedDepartureDate}`)
 							if (!oldEstimatedDepartureDate.isSame(newEstimatedDepartureDate)) {
 								console.log(`ETD CHANGE FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}`);
