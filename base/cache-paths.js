d014139c976d143670d42bfe6add64dd65ba7289
Kennys Ng <kennys.ng@swivelsoftware.com>
2018-10-24 15:52:37 +0800

    update cache-paths

--- base/cache-paths.js
+++ base/cache-paths.js
@@ -6,43 +6,38 @@ module.exports = {
 	
 	paths: [
 		{
-			pathRegExp: /reports\/uber[A-Za-z0-9\/\#\{\}\"\:\-\[\]\=,\&\%\?\ ]*groupBy/,
-			method: "GET",
-			expiryMinutes: 60
-		},
-		{
-			path: "/reports/uber",
-			method: "POST",
-			bodyRegExp: /groupBy/,
-			expiryMinutes: 60
-		},
-		{
-			path: "/reports/tag",
-			method: "GET",
+			path: "/reports/uber-division",
+			method: ["GET", "POST"],
 			expiryMinutes: 1440
 		},
 		{
-			path: "/reports/uber-summary",
-			method: "GET",
-			expiryMinutes: 60
+			path: "/reports/uber",
+			body: "offset",
+			method: "POST",
+			expiryMinutes: 15
 		},
 		{
-			path: "/reports/top-x",
-			method: "GET",
-			expiryMinutes: 60
-		},
-		{
-			path: "/reports/yoy-by-month",
-			method: "GET",
+			path: "/reports/uber",
+			method: ["GET", "POST"],
 			expiryMinutes: 60
 		},
 		{
 			path: "/reports/shipment-status",
-			method: "GET",
-			expiryMinutes: 60
+			method: ["GET", "POST"],
+			expiryMinutes: 15
 		},
 		{
-			path: "/fm3k",
+			path: "/reports/alert-details",
+			method: ["GET", "POST"],
+			expiryMinutes: 5
+		},
+		{
+			path: "/reports/alerts",
+			method: ["GET", "POST"],
+			expiryMinutes: 5
+		},
+		{
+			path: "/fm3k/",
 			method: "GET",
 			expiryMinutes: 60
 		}
