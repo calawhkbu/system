a81283fd2a6ab685b979bc731438b710f21dd3c6
Kennys Ng <kennys.ng@swivelsoftware.com>
2018-10-24 16:10:01 +0800

    update cache-paths

--- base/cache-paths.js
+++ base/cache-paths.js
@@ -5,37 +5,57 @@ module.exports = {
 	],
 	
 	paths: [
+		// list of divisions
 		{
 			path: "/reports/uber-division",
 			method: ["GET", "POST"],
 			expiryMinutes: 1440
 		},
+
+		// metadata
+		{
+			pathRegExp: /\/reports\/uber(.*?)\/meta/,
+			method: "GET",
+			expiryMinutes: 1440
+		},
+
+		// uber for AdhocTool
 		{
 			path: "/reports/uber",
 			body: "offset",
 			method: "POST",
 			expiryMinutes: 15
 		},
+
+		// uber for Dashboard
 		{
 			path: "/reports/uber",
 			method: ["GET", "POST"],
 			expiryMinutes: 60
 		},
+
+		// shipment-status for Dashboard
 		{
 			path: "/reports/shipment-status",
 			method: ["GET", "POST"],
 			expiryMinutes: 15
 		},
+
+		// alert-details for AdhocTool
 		{
 			path: "/reports/alert-details",
 			method: ["GET", "POST"],
 			expiryMinutes: 5
 		},
+
+		// alerts for Dashboard
 		{
 			path: "/reports/alerts",
 			method: ["GET", "POST"],
 			expiryMinutes: 5
 		},
+
+		// FM3K APIs for Dashboard
 		{
 			path: "/fm3k/",
 			method: "GET",
