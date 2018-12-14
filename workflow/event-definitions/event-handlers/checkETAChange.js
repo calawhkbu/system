f34d2212fbb06c5b7b8760a94bf4a39dd7b30422
Ken Chan <ken.chan@swivelsoftware.com>
2018-12-14 10:59:38 +0800

    fix etd and eta problem

--- workflow/event-definitions/event-handlers/checkETAChange.js
+++ workflow/event-definitions/event-handlers/checkETAChange.js
@@ -5,41 +5,42 @@ function CheckETA() {
 		console.log(`[Event Triggered] Check ETA Change for master ${data.data.masterNo}`)
 		if (data.data.estimatedArrivalDate != null && helper.moment.isDate(data.data.estimatedArrivalDate)) {
 			console.log(`[Master NO:${data.data.masterNo}] NEW ETA: ${data.data.estimatedArrivalDate}`)
-			if (data.oldData.estimatedArrivalDate == null) {
-				var promise = new Promise(function (resolve) {
-					helper.persistence.models.bill.findOne({ where:{ customerId: data.data.customerId, masterNo: data.data.masterNo } })
-						.then((bill) => {
-							var oldEstimatedArrivalDate = helper.moment(bill.estimatedArrivalDate);
-							var newEstimatedArrivalDate = helper.moment(data.data.estimatedArrivalDate);
-							console.log(`[Master NO:${data.data.masterNo}] Compare with FM3000`);
-							console.log(`[Master NO:${data.data.masterNo}] OLD ETA: ${oldEstimatedArrivalDate}, OLD DATA ETA: ${helper.moment(data.oldData.estimatedArrivalDate)}`)
-							console.log(`[Master NO:${data.data.masterNo}] NEW ETA: ${newEstimatedArrivalDate}`)
-							if (!oldEstimatedArrivalDate.isSame(newEstimatedArrivalDate)) {
-								console.log(`ETA Change FOR BILL ${data.data.masterNo} CUSTOMERID ${data.data.customerId}`);
-								return resolve("DELAY");
-							} else {
+			return data.oldData.estimatedArrivalDate == null
+				? (// Compare ETA from FM3000 and ETA from YunDang this time
+					new Promise((resolve) => {
+						helper.persistence.models.bill.findOne({ where:{ customerId: data.data.customerId, masterNo: data.data.masterNo } })
+							.then((bill) => {
+								var oldEstimatedArrivalDate = helper.moment(bill.estimatedArrivalDate);
+								var newEstimatedArrivalDate = helper.moment(data.data.estimatedArrivalDate);
+								console.log(`[Master NO:${data.data.masterNo}] Compare with FM3000`);
+								console.log(`[Master NO:${data.data.masterNo}] OLD ETA: ${oldEstimatedArrivalDate}, OLD DATA ETA: ${helper.moment(data.oldData.estimatedArrivalDate)}`)
+								console.log(`[Master NO:${data.data.masterNo}] NEW ETA: ${newEstimatedArrivalDate}`)
+								if (!oldEstimatedArrivalDate.isSame(newEstimatedArrivalDate)) {
+									console.log(`ETA Change FOR BILL ${data.data.masterNo} CUSTOMERID ${data.data.customerId}`);
+									return resolve("DELAY");
+								}
 								return resolve("DATESET");
-							}
-						})
-				})
-				return promise;
-			} else {
-				var oldEstimatedArrivalDate = helper.moment(data.oldData.estimatedArrivalDate);
-				var newEstimatedArrivalDate = helper.moment(data.data.estimatedArrivalDate);
-				console.log(`[Master NO:${data.data.masterNo}] Compare with Old Data`);
-				console.log(`[Master NO:${data.data.masterNo}] OLD ETA: ${oldEstimatedArrivalDate}`)
-				console.log(`[Master NO:${data.data.masterNo}] NEW ETA: ${newEstimatedArrivalDate}`)
-				if (!oldEstimatedArrivalDate.isSame(newEstimatedArrivalDate)) {
-					console.log(`ETA Change FOR BILL ${data.data.masterNo} CUSTOMERID ${data.data.customerId}`);
-					return "DELAY";
-				} else {
-					return "DATESET";
-				}
-			}
-		} else {
-			console.log(`[Master NO:${data.data.masterNo}] ETA not changed`)
-			return "DATESET";
+							})
+							.catch(e => console.error(e) && resolve("ERROR"))
+					})
+				)
+				: (// Compare ETA from YunDang this time and ETA from YunDang last time
+					new Promise((resolve) => {
+						var oldEstimatedArrivalDate = helper.moment(data.oldData.estimatedArrivalDate);
+						var newEstimatedArrivalDate = helper.moment(data.data.estimatedArrivalDate);
+						console.log(`[Master NO:${data.data.masterNo}] Compare with Old Data`);
+						console.log(`[Master NO:${data.data.masterNo}] OLD ETA: ${oldEstimatedArrivalDate}`)
+						console.log(`[Master NO:${data.data.masterNo}] NEW ETA: ${newEstimatedArrivalDate}`)
+						if (!oldEstimatedArrivalDate.isSame(newEstimatedArrivalDate)) {
+							console.log(`ETA Change FOR BILL ${data.data.masterNo} CUSTOMERID ${data.data.customerId}`);
+							return resolve("DELAY");
+						}
+						return resolve("DATESET");
+					})
+				);
 		}
+		console.log(`[Master NO:${data.data.masterNo}] ETA not changed`)
+		return "DATESET";
 	}
 }
 
--- workflow/event-definitions/event-handlers/checkETDChange.js
+++ workflow/event-definitions/event-handlers/checkETDChange.js
@@ -5,41 +5,42 @@ function CheckETD() {
 		console.log(`[Event Triggered] Check ETD Change for master ${data.data.masterNo}`)
 		if (data.data.estimatedDepartureDate != null && helper.moment.isDate(data.data.estimatedDepartureDate)) {
 			console.log(`[Master NO:${data.data.masterNo}] NEW ETD: ${data.data.estimatedDepartureDate}`)
-			if (data.oldData.estimatedDepartureDate == null) {
-				var promise = new Promise(function (resolve) {
-					helper.persistence.models.bill.findOne({ where:{ customerId: data.data.customerId, masterNo: data.data.masterNo } })
-						.then((bill) => {
-							var oldEstimatedDepartureDate = helper.moment(bill.estimatedDepartureDate);
-							var newEstimatedDepartureDate = helper.moment(data.data.estimatedDepartureDate);
-							console.log(`[Master NO:${data.data.masterNo}] Compare with FM3000`);
-							console.log(`[Master NO:${data.data.masterNo}] OLD ETD: ${oldEstimatedDepartureDate}, OLD DATA ETD: ${helper.moment(data.oldData.estimatedDepartureDate)}`)
-							console.log(`[Master NO:${data.data.masterNo}] NEW ETD: ${newEstimatedDepartureDate}`)
-							if (!oldEstimatedDepartureDate.isSame(newEstimatedDepartureDate)) {
-								console.log(`ETD CHANGE FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}`);
-								return resolve("DELAY");
-							} else {
+			return data.oldData.estimatedDepartureDate == null
+				? (// Compare ETA from FM3000 and ETA from YunDang this time
+					new Promise(resolve => {
+						helper.persistence.models.bill.findOne({ where:{ customerId: data.data.customerId, masterNo: data.data.masterNo } })
+							.then((bill) => {
+								var oldEstimatedDepartureDate = helper.moment(bill.estimatedDepartureDate);
+								var newEstimatedDepartureDate = helper.moment(data.data.estimatedDepartureDate);
+								console.log(`[Master NO:${data.data.masterNo}] Compare with FM3000`);
+								console.log(`[Master NO:${data.data.masterNo}] OLD ETD: ${oldEstimatedDepartureDate}, OLD DATA ETD: ${helper.moment(data.oldData.estimatedDepartureDate)}`)
+								console.log(`[Master NO:${data.data.masterNo}] NEW ETD: ${newEstimatedDepartureDate}`)
+								if (!oldEstimatedDepartureDate.isSame(newEstimatedDepartureDate)) {
+									console.log(`ETD CHANGE FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}`);
+									return resolve("DELAY");
+								}
 								return resolve("DATESET");
-							}
-						})
-				})
-				return promise;
-			} else {
-				var oldEstimatedDepartureDate = helper.moment(data.oldData.estimatedDepartureDate);
-				var newEstimatedDepartureDate = helper.moment(data.data.estimatedDepartureDate);
-				console.log(`[Master NO:${data.data.masterNo}] Compare with Old Data`);
-				console.log(`[Master NO:${data.data.masterNo}] OLD ETD: ${oldEstimatedDepartureDate}`)
-				console.log(`[Master NO:${data.data.masterNo}] NEW ETD: ${newEstimatedDepartureDate}`)
-				if (!oldEstimatedDepartureDate.isSame(newEstimatedDepartureDate)) {
-					console.log(`ETD CHANGE FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}`);
-					return "DELAY";
-				} else {
-					return "DATESET";
-				}
-			}
-		} else {
-			console.log(`[Master NO:${data.data.masterNo}] ETD not changed`)
-			return "DATESET";
+							})
+							.catch(e => console.error(e) && resolve("ERROR"))
+					})
+				)
+				: (// Compare ETA from YunDang this time and ETA from YunDang last time
+					new Promise(resolve => {
+						var oldEstimatedDepartureDate = helper.moment(data.oldData.estimatedDepartureDate);
+						var newEstimatedDepartureDate = helper.moment(data.data.estimatedDepartureDate);
+						console.log(`[Master NO:${data.data.masterNo}] Compare with Old Data`);
+						console.log(`[Master NO:${data.data.masterNo}] OLD ETD: ${oldEstimatedDepartureDate}`)
+						console.log(`[Master NO:${data.data.masterNo}] NEW ETD: ${newEstimatedDepartureDate}`)
+						if (!oldEstimatedDepartureDate.isSame(newEstimatedDepartureDate)) {
+							console.log(`ETD CHANGE FOR BILL ${data.data.masterNo} CUSTOMER-ID ${data.data.customerId}`);
+							return resolve("DELAY");
+						}
+						return resolve("DATESET");
+					})
+				)
 		}
+		console.log(`[Master NO:${data.data.masterNo}] ETD not changed`)
+		return "DATESET";
 	}
 }
 
