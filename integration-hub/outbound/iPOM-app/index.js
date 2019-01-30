ca70c46db3696d924021388e71c66d8d91ec2ced
Ken Chan <ken.chan@swivelsoftware.com>
2019-01-30 15:29:26 +0800

    send po to ipom

--- integration-hub/outbound/iPOM-app/index.js
+++ integration-hub/outbound/iPOM-app/index.js
@@ -5,23 +5,38 @@ function iPOMHandler () {
       .then(customer => {
         if (customer && customer.configuration && customer.configuration.webService["purchaseOrder"]) {
           let api = customer.configuration.webService["purchaseOrder"].api;
-          let reqPayLoad = JSON.stringify(purchaseOrder);
+          console.log(api)
+          let reqPayLoad = {
+            data: { purchaseOrder: purchaseOrder },
+            headers: { "Content-Type": "application/json" }
+          };
           try {
-            console.log(api, reqPayLoad)
-            helper.restClient.post(api, { data: reqPayLoad }, (postData, response) => {
+            helper.restClient.post(api, reqPayLoad, (postData, response) => {
               if (Buffer.isBuffer(postData)) {
                 postData = postData.toString('utf8');
               }
               if (postData && postData.trim().length > 0) {
+                const response = JSON.parse(postData.trim())[0]
+                if (response.status === 200) {
+                  console.log('success send to ipom')
+                  helper.saveLog(appId, api, 'purchaseOrder', purchaseOrder.id, reqPayLoad, JSON.stringify(postData), null);
+                } else {
+                  helper.emailer.sendFreeMail({
+                    to: ["ken.chan@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+                    from: "administrator@swivelsoftware.com",
+                    subject: `TEST - Failed to post purchaseOrder to iPOM [DATA SENT OUT]`,
+                    html: `<p>PO:</p><br/><p>${JSON.stringify(reqPayLoad)}</p><p>Data Return:</p><br/><p>${JSON.stringify(postData)}</p>`
+                  }, {purchaseOrder: purchaseOrder});
+                  helper.saveLog(appId, api, 'purchaseOrder', purchaseOrder.id, reqPayLoad, null, JSON.stringify(postData));
+                }
+              } else {
                 helper.emailer.sendFreeMail({
                   to: ["ken.chan@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
                   from: "administrator@swivelsoftware.com",
                   subject: `TEST - Failed to post purchaseOrder to iPOM [DATA SENT OUT]`,
-                  html: `<p>PO:${reqPayLoad}</p><p>Data Return:${JSON.stringify(postData)}</p>`
+                  html: `<p>PO:</p><br/><p>${JSON.stringify(reqPayLoad)}</p><p>Data Return:</p><br/><p>${JSON.stringify(postData)}</p>`
                 }, {purchaseOrder: purchaseOrder});
                 helper.saveLog(appId, api, 'purchaseOrder', purchaseOrder.id, reqPayLoad, null, JSON.stringify(postData));
-              } else {
-                helper.saveLog(appId, api, 'purchaseOrder', purchaseOrder.id, reqPayLoad, JSON.stringify(postData), null);
               }
             })
           } catch (e) {
@@ -30,7 +45,7 @@ function iPOMHandler () {
               to: ["ken.chan@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
               from: "administrator@swivelsoftware.com",
               subject: `TEST - Failed to post purchaseOrder to iPOM [ERROR]`,
-              html: `<p>PO:${reqPayLoad}</p><p>Error: ${JSON.stringify(e.message)}</p><p>${e.stack}</p>`
+              html: `<p>PO:</p><br/><p>${reqPayLoad}</p><p>Error:</p><br/><p>${JSON.stringify(e.message)}</p><p>${e.stack}</p>`
             }, {purchaseOrder: purchaseOrder});
             helper.saveLog(appId, api, 'purchaseOrder', purchaseOrder.id, reqPayLoad, null, JSON.stringify(e));
           }
