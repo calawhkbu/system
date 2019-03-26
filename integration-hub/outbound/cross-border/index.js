1c8d9744c84f5fb1cad3a371f5872821f84bad02
Ken Chan <ken.chan@swivelsoftware.com>
2019-03-26 09:41:46 +0800

    test diff and fix lazed change

--- integration-hub/outbound/cross-border/index.js
+++ integration-hub/outbound/cross-border/index.js
@@ -1,5 +1,4 @@
 function lazadaNotificationHandler () {
-  var statusMapping = require("./status-mapping.js");
   var getUrl = function (helper, action){
     // let hmac = helper.crypto.createHmac("sha512", new Buffer(constants.TOKEN, 'utf-8'));
     // let signed = hmac.update(new Buffer(encodeURIComponent(message), 'utf-8')).digest("hex");
@@ -13,16 +12,23 @@ function lazadaNotificationHandler () {
       billCargoTracking: billCargoTracking
     }
   }
+  var compareValue = function (oldValue, newValue) {
+    console.log(oldValue, newValue, oldValue !== newValue)
+    return oldValue !== newValue
+  }
+  var compareObject = function (oldObject, newObject, diff) {
+    return diff(oldObject, newObject).reduce(
+      (difference, diffValue) => {
+        if (diffValue.lhs && diffValue.rhs) {
+          console.log('path', JSON.stringify(path))
+          console.log(difference, difference || compareValue(diffValue.lhs, diffValue.rhs))
+          difference = difference || compareValue(diffValue.lhs, diffValue.rhs)
+        }
+      },
+      false
+    )
+  }
   var compare = function (oldTracking, newTracking, diff) {
-    console.log(oldTracking.lastStatus, newTracking.lastStatus, oldTracking.lastStatus !== newTracking.lastStatus)
-    console.log(oldTracking.lastStatusCode, newTracking.lastStatusCode, oldTracking.lastStatusCode !== newTracking.lastStatusCode)
-    console.log(oldTracking.lastStatusDate, newTracking.lastStatusDate, oldTracking.lastStatusDate !== newTracking.lastStatusDate)
-    console.log(oldTracking.estimatedDepartureDate, newTracking.estimatedDepartureDate, oldTracking.estimatedDepartureDate !== newTracking.estimatedDepartureDate)
-    console.log(oldTracking.estimatedArrivalDate, newTracking.estimatedArrivalDate, oldTracking.estimatedArrivalDate !== newTracking.estimatedArrivalDate)
-    console.log(oldTracking.actualDepartureDate, newTracking.actualDepartureDate, oldTracking.actualDepartureDate !== newTracking.actualDepartureDate)
-    console.log(oldTracking.actualArrivalDate, newTracking.actualArrivalDate, oldTracking.actualArrivalDate !== newTracking.actualArrivalDate)
-    console.log(JSON.stringify(diff(oldTracking.lastStatusDetails, newTracking.lastStatusDetails)))
-
     return oldTracking.lastStatus !== newTracking.lastStatus
     || oldTracking.lastStatusCode !== newTracking.lastStatusCode
     || oldTracking.lastStatusDate !== newTracking.lastStatusDate
@@ -40,8 +46,13 @@ function lazadaNotificationHandler () {
     var newTracking = params.data.data;
     var oldTransform = transform(oldTracking.lastStatusDetails);
     var newTransform = transform(newTracking.lastStatusDetails);
-    console.log(JSON.stringify(helper.diff(oldTransform, newTransform)))
-    if (newTracking && newTracking.lastStatusDetails && compare(oldTracking, newTracking, helper.diff)) { // IF STATUS UPDATED
+    console.log('result', compareObject(oldTransform, newTransform, helper.diff))
+    if (
+      newTracking
+      && newTracking.lastStatusDetails
+      // check if status change
+      && compare(oldTracking, newTracking, helper.diff)
+    ) {
       try {
         helper.restClient.post(url, { data: newTransform, headers: { "Content-Type": "application/json" } }, (postData) => {
           if(Buffer.isBuffer(postData)){
@@ -49,12 +60,18 @@ function lazadaNotificationHandler () {
           }
           if (postData.success) {
             helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), JSON.stringify(postData), null);
+            helper.emailer.sendFreeMail({
+              to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+              from: "administrator@swivelsoftware.com",
+              subject: `Success to send data to lazada`,
+              html: ``
+            }, {});
           } else {
             helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(postData));
             helper.emailer.sendFreeMail({
               to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
               from: "administrator@swivelsoftware.com",
-              subject: `Fial to send data to lazada (MAWB: ${newTracking.masterNo})`,
+              subject: `Lazada return not success (MAWB: ${newTracking.masterNo})`,
               html: `<p>Data Return: ${JSON.stringify(postData)}</p>`
             }, {});
           }
