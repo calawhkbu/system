2aec5b947fb4d37447c94fb2618ee65c2d554cb8
Ken Chan <ken.chan@swivelsoftware.com>
2019-04-09 16:52:24 +0800

    Fix yundang change

--- integration-hub/outbound/cross-border/index.js
+++ integration-hub/outbound/cross-border/index.js
@@ -1,8 +1,9 @@
 function lazadaNotificationHandler () {
-  var getUrl = function (helper, action){
-    // let hmac = helper.crypto.createHmac("sha512", new Buffer(constants.TOKEN, 'utf-8'));
-    // let signed = hmac.update(new Buffer(encodeURIComponent(message), 'utf-8')).digest("hex");
-    return `http://cbestaging.lazada.com/lzdelg-gw/cb-ftt/mawbsub/receive?action=updateStatus&timestamp=${helper.moment().toISOString(true)}&userid=Swivel&signature=6700f708944732491dfebdf559422923c818e3c1fbd9df15e7e6688e097a81d3`;
+  var getUrl = function (helper){
+    var time = helper.moment().utcOffset("+08:00").format('YYYY-MM-DDTHH:mm:ss.SSSZZ')
+    var message = encodeURIComponent(`action=updateStatus&timestamp=${time}&userid=Swivel`)
+    var signed = helper.crypto.createHmac("sha256", 'LJADLFUADKJLKUW').update(message).digest("hex");
+    return `http://cbestaging.lazada.com/lzdelg-gw/cb-ftt/mawbsub/receive?${message}&signature=${signed}`;
   }
   var transform = function (details) {
     return {
@@ -24,74 +25,101 @@ function lazadaNotificationHandler () {
     return oldValue !== newValue
   }
   var compareObject = function (oldObject, newObject, diff, moment) {
-    return diff(oldObject, newObject).reduce(
-      (difference, diffValue) => {
-        if (diffValue.lhs && diffValue.rhs) {
-          if (moment.isDate(diffValue.lhs) || moment.isDate(diffValue.rhs)) {
-            console.log(moment(diffValue.lhs), moment(diffValue.rhs))
-            console.log(moment(diffValue.lhs).isSame(moment(diffValue.rhs)))
-            difference = difference || moment(diffValue.lhs).isSame(moment(diffValue.rhs))
-          } else {
-            difference = difference || compareValue(diffValue.lhs, diffValue.rhs)
+    const d = diff(oldObject, newObject)
+    if (d) {
+      return d.reduce(
+        (difference, diffValue) => {
+          console.log(`[LAZADA] checking ... (${diffValue.lhs}, ${diffValue.rhs})`)
+          if (diffValue.lhs && diffValue.rhs) {
+            if (moment.isDate(diffValue.lhs) || moment.isDate(diffValue.rhs)) {
+              difference = difference || moment(diffValue.lhs).isSame(moment(diffValue.rhs))
+            } else {
+              difference = difference || compareValue(diffValue.lhs, diffValue.rhs)
+            }
           }
-        }
-        return difference
-      },
-      false
-    )
-  }
-  var compare = function (oldTracking, newTracking, diff) {
-    return oldTracking.lastStatus !== newTracking.lastStatus
-    || oldTracking.lastStatusCode !== newTracking.lastStatusCode
-    || oldTracking.lastStatusDate !== newTracking.lastStatusDate
-    || oldTracking.estimatedDepartureDate !== newTracking.estimatedDepartureDate
-    || oldTracking.estimatedArrivalDate !== newTracking.estimatedArrivalDate
-    || oldTracking.actualDepartureDate !== newTracking.actualDepartureDate
-    || oldTracking.actualArrivalDate !== newTracking.actualArrivalDate
-    || diff(oldTracking.lastStatusDetails, newTracking.lastStatusDetails);
+          return difference
+        },
+        false
+      )
+    }
+    return false
   }
   this.handle = function (appId, params, helper) {
     console.log('[LAZADA] Start sending data to lazada')
-    var url = getUrl(helper, params.handlerParameters.action);
+    var url = getUrl(helper);
     var entity = params.data.modelName;
     var oldTracking = params.data.oldData;
     var newTracking = params.data.data;
-    var oldTransform = transform(oldTracking.lastStatusDetails);
-    var newTransform = transform(newTracking.lastStatusDetails);
-    console.log('result', compareObject(oldTransform, newTransform, helper.diff, helper.moment))
-    if (
-      newTracking
-      && newTracking.lastStatusDetails
-      // check if status change
-      && compare(oldTracking, newTracking, helper.diff)
-    ) {
-      try {
-        helper.restClient.post(url, { data: newTransform, headers: { "Content-Type": "application/json" } }, (postData) => {
-          if(Buffer.isBuffer(postData)){
-            postData = postData.toString('utf8');
-          }
-          if (postData && postData.response && postData.response.success) {
-            helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), JSON.stringify(postData), null);
-          } else {
-            helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(postData));
-            helper.emailer.sendFreeMail({
-              to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
-              from: "administrator@swivelsoftware.com",
-              subject: `Lazada return not success (MAWB: ${newTracking.masterNo})`,
-              html: `<p>Data Return: ${JSON.stringify(postData)}</p>`
-            }, {});
-          }
-        })
-      } catch (e) {
-        helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(e));
-        helper.emailer.sendFreeMail({
-          to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
-          from: "administrator@swivelsoftware.com",
-          subject: `Fail to send Status update to Lazada (MAWB: ${newTracking.masterNo})`,
-          html: `<p>Data Return: ${JSON.stringify(e)}</p>`
-        }, {});
+    if (oldTracking && newTracking && oldTracking.lastStatusDetails && newTracking.lastStatusDetails) {
+      var oldTransform = transform(oldTracking.lastStatusDetails);
+      var newTransform = transform(newTracking.lastStatusDetails);
+      if (compareObject(oldTransform, newTransform, helper.diff, helper.moment)) {
+        console.log('[LAZADA] DATA Changed')
+        try {
+          helper.restClient.post(url, { data: newTransform, headers: { "Content-Type": "application/json" } }, (postData) => {
+            if (Buffer.isBuffer(postData)) {
+              postData = postData.toString('utf8');
+            }
+            console.log(`[LAZADA] return ${JSON.stringify(postData)}`)
+            if (postData && postData.response && postData.response.success) {
+              helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), JSON.stringify(postData), null);
+            } else {
+              helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(postData));
+              helper.emailer.sendFreeMail({
+                to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+                from: "administrator@swivelsoftware.com",
+                subject: `Lazada return not success (MAWB: ${newTracking.masterNo})`,
+                html: `<p>Data Return: ${JSON.stringify(postData)}</p>`
+              }, {});
+            }
+          })
+        } catch (e) {
+          helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(e));
+          helper.emailer.sendFreeMail({
+            to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+            from: "administrator@swivelsoftware.com",
+            subject: `Lazada return not success (MAWB: ${newTracking.masterNo})`,
+            html: `<p>Data Return: ${JSON.stringify(e)}</p>`
+          }, {});
+        }
       }
     }
+
+    // if (
+    //   newTracking
+    //   && newTracking.lastStatusDetails
+    //   // check if status change
+    //   && compare(oldTracking, newTracking, helper.diff)
+    // ) {
+    //   try {
+    //     console.log(url)
+    //     console.log(JSON.stringify(newTransform))
+    //     // helper.restClient.post(url, { data: newTransform, headers: { "Content-Type": "application/json" } }, (postData) => {
+    //     //   if(Buffer.isBuffer(postData)){
+    //     //     postData = postData.toString('utf8');
+    //     //   }
+    //     //   if (postData && postData.response && postData.response.success) {
+    //     //     helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), JSON.stringify(postData), null);
+    //     //   } else {
+    //     //     helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(postData));
+    //     //     helper.emailer.sendFreeMail({
+    //     //       to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+    //     //       from: "administrator@swivelsoftware.com",
+    //     //       subject: `Lazada return not success (MAWB: ${newTracking.masterNo})`,
+    //     //       html: `<p>Data Return: ${JSON.stringify(postData)}</p>`
+    //     //     }, {});
+    //     //   }
+    //     // })
+    //   } catch (e) {
+    //     helper.saveLog(appId, url, entity, newTracking.id, JSON.stringify(newTransform), null, JSON.stringify(e));
+    //     helper.emailer.sendFreeMail({
+    //       to: ["ken.chan+lazada@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+    //       from: "administrator@swivelsoftware.com",
+    //       subject: `Fail to send Status update to Lazada (MAWB: ${newTracking.masterNo})`,
+    //       html: `<p>Data Return: ${JSON.stringify(e)}</p>`
+    //     }, {});
+    //   }
+    // }
   }
 }
 
