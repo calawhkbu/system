ab31d11f484009a2b10e71baab4a6edf7b6bef5f
Ken Chan <ken.chan@swivelsoftware.com>
2019-04-17 15:03:07 +0800

    Ene use curl

--- integration-hub/outbound/ene-app/index.js
+++ integration-hub/outbound/ene-app/index.js
@@ -25,32 +25,40 @@ function ENEAPPHandler () {
       })
     )
       .then((purchaseOrders) => {
-        let reqPayLoad = {
-          data: { ...booking, purchaseOrders },
-          headers: {
-            "Content-Type": "application/json",
-            "Token": "Bearer kRXEe3eVDJJw/3bnhtzTOvLf4DlKjM6AzWrUGd+42vU="
-          }
-        };
+        const reqPayLoad = { ...booking, purchaseOrders }
+        console.log('[ENE]', url)
         console.log('[ENE]', JSON.stringify(reqPayLoad))
         try {
-          console.log('[ENE] Ready...')
-          return helper.restClient.post(url, reqPayLoad, (postData) => {
-            console.log('[ENE] RETURN')
-            if (Buffer.isBuffer(postData)) {
-              postData = postData.toString('utf8');
-            }
-            console.log('[ENE]', `return ${postData}`)
-            helper.saveLog(appId, url, 'booking', booking.id, JSON.stringify(reqPayLoad), postData, null);
+          var curl = new helper.Curl();
+          curl.setOpt(helper.Curl.option.URL, 'http://tzumimod.com/controllers/API/api.php')
+          curl.setOpt(helper.Curl.option.POSTFIELDS, helper.querystring.stringify(reqPayLoad))
+          curl.setOpt(helper.Curl.option.HTTPHEADER, [
+            'Content-Type:application/json',
+            'Token: Bearer kRXEe3eVDJJw/3bnhtzTOvLf4DlKjM6AzWrUGd+42vU='
+          ]);
+          curl.on( 'end', function( statusCode, body, headers ) {
+            helper.saveLog(appId, url, 'booking', booking.id, JSON.stringify(reqPayLoad), JSON.stringify(body), null);
             helper.emailer.sendFreeMail({
               to: ["ken.chan+ene@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
               from: "administrator@swivelsoftware.com",
               subject: `TEST-ENEAPP [SUCCESS]`,
-              html: `${reqPayLoad.data}`
+              html: `${JSON.stringify(body)}`
             }, {});
-          })
+            this.close();
+          });
+          curl.on( 'error', function( err, curlErrorCode ) {
+            helper.saveLog(appId, url, 'booking', booking.id, JSON.stringify(reqPayLoad), null, JSON.stringify(e));
+            helper.emailer.sendFreeMail({
+              to: ["ken.chan+ene@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
+              from: "administrator@swivelsoftware.com",
+              subject: `TEST-ENEAPP [FAIL]`,
+              html: `${JSON.stringify(e)}`
+            }, {});
+            this.close();
+          });
+          return curl.perform();
         } catch (e) {
-          console.error(JSON.stringify(e))
+          console.error(e)
           helper.saveLog(appId, url, 'booking', booking.id, JSON.stringify(reqPayLoad), null, JSON.stringify(e));
           helper.emailer.sendFreeMail({
             to: ["ken.chan+ene@swivelsoftware.com"].join(','),   //TODO REMOVE HARD-CODED
