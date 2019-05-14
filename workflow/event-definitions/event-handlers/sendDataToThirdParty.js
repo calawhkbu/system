5019e7379cd7cc4e7009addd3ab1e58d1e91877c
Kennys Ng <kennys.ng@swivelsoftware.com>
2019-05-14 17:50:08 +0800

    test

--- workflow/event-definitions/event-handlers/sendDataToThirdParty.js
+++ workflow/event-definitions/event-handlers/sendDataToThirdParty.js
@@ -2,6 +2,7 @@ function sendDataToThirdParty() {
 
 	this.handle = function(definition, data, handlerParameters, helper) {
 		if (handlerParameters.appId) {
+			console.log('sendDataToThirdParty ' + handlerParameters.appId)
 			helper.integrationHub.outboundAPIHandler(handlerParameters.appId, handlerParameters.customerId || data.customerId, {
 				definition: definition,
 				data: data,
