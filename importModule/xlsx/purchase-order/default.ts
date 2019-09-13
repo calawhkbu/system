4d60569456fef18dc7a40fad15cb1be92d3e2112
Ken Chan <ken.chan@swivelsoftware.com>
2019-09-13 20:44:33 +0800

    fixing

---
+++ importModule/xlsx/purchase-order/default.ts
@@ -0,0 +1,12 @@
+import { BaseExcelParser } from 'modules/parser/parser/excel'
+export const templateFormat =  {
+
+}
+
+export default class DefaultParser extends BaseExcelParser {
+  async import(base64String: string) {
+    const response = super.import(base64String)
+    console.log(response)
+    return response
+  }
+}
---
+++ integrationHubModule/outbound/ipom-po.ts
@@ -0,0 +1,20 @@
+
+export default {
+  method: 'POST', // 'GET'|'POST'|'PUT'|'DELETE'|'HEAD'|'OPTIONS'
+  getUrl: (headers: any, body: any) => {
+    return 'http://ecxipomapi_uat.swivelsoftware.com/ipomapi.asmx'
+  },
+  requestHandler: (headers: any, body: any) => {
+    return {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        pojson: body
+      }
+    }
+  },
+  responseHandler: (response: { responseBody: any; responseOptions: any }) => {
+    return response
+  },
+}
