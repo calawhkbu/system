6ea81447909141427e30d0c96afee827f0920f6a
Kennys Ng <kennys.ng@swivelsoftware.com>
2019-09-26 17:12:48 +0800

    tonnage jql (incomplete)

--- reportingModule/card/shipment-ae-carrier-weight-month.ts
+++ reportingModule/card/shipment-ae-carrier-weight-month.ts
@@ -1,17 +1,15 @@
 import {
   AndExpressions,
   BinaryExpression,
-  Column,
   ColumnExpression,
   CreateTableJQL,
   FromTable,
   FunctionExpression,
   InsertJQL,
+  Value,
   Query,
   ResultColumn,
-  Value,
 } from 'node-jql'
-import { parseCode } from 'utils/function'
 
 const months = [
   'Total',
@@ -28,125 +26,99 @@ const months = [
   'November',
   'December',
 ]
-const types = ['F_GW', 'F_CW', 'R_GW', 'R_CW']
+const types = ['GW', 'CW']
 
-function prepareParams(type_: 'F' | 'R'): Function {
-  const fn = function(require, session, params) {
-    // import
-    const moment = require('moment')
+export default [
+  [
+    // process params
+    function(require, session, params) {
+      // import
+      const moment = require('moment')
 
-    // limit/extend to 1 year
-    const subqueries = (params.subqueries = params.subqueries || {})
-    const year = (subqueries.data ? moment() : moment(subqueries.date.from, 'YYYY-MM-DD')).year()
-    subqueries.date.from = moment()
-      .year(year)
-      .startOf('year')
-      .format('YYYY-MM-DD')
-    subqueries.date.to = moment()
-      .year(year)
-      .endOf('year')
-      .format('YYYY-MM-DD')
+      // limit/extend to 1 year
+      const subqueries = (params.subqueries = params.subqueries || {})
+      const year = (subqueries.data ? moment() : moment(subqueries.date.from, 'YYYY-MM-DD')).year()
+      subqueries.date.from = moment()
+        .year(year)
+        .startOf('year')
+        .format('YYYY-MM-DD')
+      subqueries.date.to = moment()
+        .year(year)
+        .endOf('year')
+        .format('YYYY-MM-DD')
 
-    // AE
-    subqueries.moduleType = { value: 'AIR' }
-    subqueries.boundType = { value: 'O' }
+      // AE
+      subqueries.moduleType = { value: 'AIR' }
+      subqueries.boundType = { value: 'O' }
 
-    // select
-    params.fields = ['carrierCode', 'jobMonth', 'SUM(grossWeight)', 'SUM(chargeableWeight)']
+      // select
+      params.fields = ['carrierCode', 'jobMonth', 'SUM(grossWeight)', 'SUM(chargeableWeight)']
 
-    // group by
-    params.groupBy = ['carrierCode', 'jobMonth']
+      // group by
+      params.groupBy = ['carrierCode', 'jobMonth']
 
-    switch (type_) {
-      case 'F':
-        subqueries.nominatedType = { value: 'F' }
-        break
-      case 'R':
-        subqueries.nominatedType = { value: 'R' }
-        break
-    }
-
-    return params
-  }
-  let code = fn.toString()
-  code = code.replace(new RegExp('type_', 'g'), `'${type_}'`)
-  return parseCode(code)
-}
-
-// call API
-function prepareData(type: 'F' | 'R'): InsertJQL {
-  return new InsertJQL({
-    name: 'shipment',
-    columns: ['type', 'carrierCode', 'month', 'grossWeight', 'chargeableWeight'],
-    query: new Query({
-      $select: [
-        new ResultColumn(new Value(type), 'type'),
-        new ResultColumn('carrierCode'),
-        new ResultColumn(
-          new FunctionExpression('MONTHNAME', new ColumnExpression('jobMonth'), 'YYYY-MM'),
-          'month'
+      return params
+    },
+    // create temp table
+    new CreateTableJQL({
+      $temporary: true,
+      name: 'shipment',
+      $as: new Query({
+        $select: [
+          new ResultColumn('carrierCode'),
+          new ResultColumn(
+            new FunctionExpression('MONTHNAME', new ColumnExpression('jobMonth'), 'YYYY-MM'),
+            'month'
+          ),
+          new ResultColumn('grossWeight'),
+          new ResultColumn('chargeableWeight'),
+        ],
+        $from: new FromTable(
+          {
+            method: 'POST',
+            url: 'api/shipment/query/shipment',
+            columns: [
+              { name: 'carrierCode', type: 'string' },
+              { name: 'jobMonth', type: 'string' },
+              { name: 'SUM(grossWeight)', type: 'number', $as: 'grossWeight' },
+              { name: 'SUM(chargeableWeight)', type: 'number', $as: 'chargeableWeight' },
+            ],
+          },
+          'shipment'
         ),
-        new ResultColumn('grossWeight'),
-        new ResultColumn('chargeableWeight'),
-      ],
-      $from: new FromTable(
-        {
-          method: 'POST',
-          url: 'api/shipment/query/shipment',
-          columns: [
-            { name: 'carrierCode', type: 'string' },
-            { name: 'jobMonth', type: 'string' },
-            { name: 'SUM(grossWeight)', type: 'number', $as: 'grossWeight' },
-            { name: 'SUM(chargeableWeight)', type: 'number', $as: 'chargeableWeight' },
-          ],
-        },
-        'shipment'
-      ),
-    }),
-  })
-}
+      }),
+    })
+  ],
 
-// prepare Total row(s)
-function prepareTotal(type: 'F' | 'R'): InsertJQL {
-  return new InsertJQL({
+  // prepare total column
+  new InsertJQL({
     name: 'shipment',
-    columns: ['type', 'carrierCode', 'month', 'grossWeight', 'chargeableWeight'],
+    columns: ['carrierCode', 'month', 'grossWeight', 'chargeableWeight'],
     query: new Query({
       $select: [
-        new ResultColumn(new Value(type), 'type'),
-        new ResultColumn('carrierCdoe'),
+        new ResultColumn('carrierCode'),
         new ResultColumn(new Value('Total'), 'month'),
         new ResultColumn(
-          new FunctionExpression('SUM', new ColumnExpression('grossWeight')),
+          new FunctionExpression(
+            'IFNULL',
+            new FunctionExpression('SUM', new ColumnExpression('grossWeight')),
+            0
+          ),
           'grossWeight'
         ),
         new ResultColumn(
-          new FunctionExpression('SUM', new ColumnExpression('chargeableWeight')),
+          new FunctionExpression(
+            'IFNULL',
+            new FunctionExpression('SUM', new ColumnExpression('chargeableWeight')),
+            0
+          ),
           'chargeableWeight'
         ),
       ],
       $from: 'shipment',
       $group: 'carrierCode',
-      $where: new BinaryExpression(new ColumnExpression('type'), '=', type),
     }),
-  })
-}
-
-export default [
-  // prepare temp table
-  new CreateTableJQL(true, 'shipment', [
-    new Column('type', 'string'),
-    new Column('carrierCode', 'string'),
-    new Column('month', 'string'),
-    new Column('grossWeight', 'number'),
-    new Column('chargeableWeight', 'number'),
-  ]),
-
-  // prepare data
-  [prepareParams('F'), prepareData('F')],
-  prepareTotal('F'),
-  [prepareParams('R'), prepareData('R')],
-  prepareTotal('R'),
+  }),
 
   // finalize data
   new Query({
@@ -159,10 +131,7 @@ export default [
               new ResultColumn(
                 new FunctionExpression(
                   'FIND',
-                  new AndExpressions([
-                    new BinaryExpression(new ColumnExpression('month'), '=', month),
-                    new BinaryExpression(new ColumnExpression('type'), '=', type.charAt(0)),
-                  ]),
+                  new BinaryExpression(new ColumnExpression('month'), '=', month),
                   new ColumnExpression(
                     type.substr(2, 2) === 'GW' ? 'grossWeight' : 'chargeableWeight'
                   )
