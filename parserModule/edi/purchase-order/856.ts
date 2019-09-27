f1d3278be6d1cf95770dac625ea44b0b183848fd
Tomche <tom.che@swivelsoftware.com>
2019-09-27 11:48:11 +0800

    856 mina change

--- parserModule/edi/purchase-order/856.ts
+++ parserModule/edi/purchase-order/856.ts
@@ -114,7 +114,7 @@ export default class EdiParser856 extends BaseEdiParser {
     data.push(GE, IEA)
     _.set(returnJSON, 'data', data)
     // return cloneEntityJSON
-    return returnJSON
+    // return returnJSON
     const result = await super.export(returnJSON)
     return result
   }
