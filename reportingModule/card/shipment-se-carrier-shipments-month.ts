38e742f0724d5c630bd49f7be5f07c6fefa3ae34
Marco Lor <marco.lor@swivelsoftware.com>
2019-10-03 11:07:25 +0800

    copy se card

---
+++ reportingModule/card/shipment-se-carrier-shipments-month.ts
@@ -0,0 +1,134 @@
+import {
+  AndExpressions,
+  BinaryExpression,
+  ColumnExpression,
+  CreateTableJQL,
+  FromTable,
+  FunctionExpression,
+  InsertJQL,
+  Value,
+  Query,
+  ResultColumn,
+  Column,
+} from 'node-jql'
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+const types = ['shipments']
+
+function prepareParams(): Function {
+  return function(require, session, params) {
+    // import
+    const moment = require('moment')
+
+    // limit/extend to 1 year
+    const subqueries = (params.subqueries = params.subqueries || {})
+    const year = (subqueries.data ? moment() : moment(subqueries.date.from, 'YYYY-MM-DD')).year()
+    subqueries.date.from = moment()
+      .year(year)
+      .startOf('year')
+      .format('YYYY-MM-DD')
+    subqueries.date.to = moment()
+      .year(year)
+      .endOf('year')
+      .format('YYYY-MM-DD')
+
+    // AE
+    subqueries.moduleType = { value: 'SEA' }
+    subqueries.boundType = { value: 'O' }
+
+    // select
+    params.fields = ['carrierCode', 'jobMonth', 'shipments']
+
+    // group by
+    params.groupBy = ['carrierCode', 'jobMonth']
+
+    return params
+  }
+}
+
+function prepareTable(): CreateTableJQL {
+
+  return new CreateTableJQL({
+    $temporary: true,
+    name: 'shipment',
+    $as: new Query({
+      $select: [
+        new ResultColumn('carrierCode'),
+        new ResultColumn(
+          new FunctionExpression('MONTHNAME', new ColumnExpression('jobMonth'), 'YYYY-MM'),
+          'month'
+        ),
+        new ResultColumn('shipments'),
+      ],
+      $from: new FromTable(
+        {
+          method: 'POST',
+          url: 'api/shipment/query/shipment',
+          columns: [
+            { name: 'carrierCode', type: 'string' },
+            { name: 'jobMonth', type: 'string' },
+            { name: 'shipments', type: 'number' },
+          ],
+
+          data: {
+
+            filter: { carrierCodeIsNotNull: {} },
+
+          }
+        },
+        'shipment'
+      ),
+    }),
+  })
+}
+
+export default [
+
+  [prepareParams(), prepareTable()],
+
+  // finalize data
+  new Query({
+    $select: [
+      new ResultColumn('carrierCode'),
+      ...months.reduce<ResultColumn[]>((result, month) => {
+        result.push(
+          ...types.map(
+            type =>
+              new ResultColumn(
+
+                new FunctionExpression('IFNULL',
+
+                  new FunctionExpression(
+                    'FIND',
+                    new BinaryExpression(new ColumnExpression('month'), '=', month),
+                    new ColumnExpression(
+                      // only shipment
+                      type === 'shipments' ? 'shipments' : 'shipments'
+                    )
+                  ), 0),
+
+                `${month}-${type}`
+              )
+          )
+        )
+        return result
+      }, []),
+    ],
+    $from: 'shipment',
+    $group: 'carrierCode',
+  }),
+
+]
