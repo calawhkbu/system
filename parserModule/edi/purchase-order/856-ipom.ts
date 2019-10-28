07a05bdeac5b3d0352f790449679489c3bd33fda
Ken Chan <ken.chan@swivelsoftware.com>
2019-10-28 17:32:49 +0800

    fix bug

--- parserModule/edi/purchase-order/856-ipom.ts
+++ parserModule/edi/purchase-order/856-ipom.ts
@@ -116,7 +116,7 @@ export default class EdiParser856 extends BaseEdiParser {
     // return cloneEntityJSON
     // return returnJSON
     const result = await super.export(returnJSON)
-    return result
+    return [result]
   }
   async removeEmptyElementListObject(data)
   {
