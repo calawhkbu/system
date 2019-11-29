a970a5e8dd99fcaa57d08141fd73f6f18422f540
Ken Chan <ken.chan@swivelsoftware.com>
2019-11-29 17:25:24 +0800

    fix bug

--- queryModule/query-definition/code_master.ts
+++ queryModule/query-definition/code_master.ts
@@ -3,7 +3,6 @@ import {
   Query,
   FromTable,
   FunctionExpression,
-  Unknown,
   BinaryExpression,
   ColumnExpression,
   OrExpressions,
@@ -17,28 +16,21 @@ import {
 
 const query = new QueryDef(
   new Query({
-
     // one more layer of select from to prevent overriding select code_master.*
     $from : new FromTable({
-
       $as : 'code_master',
-
       table : new Query({
-
         $select : [
-          new ResultColumn(new ColumnExpression('code_master', '*'))
+          new ResultColumn(new ColumnExpression('right_code_master', '*'))
         ],
         $from : new FromTable({
           table : new Query({
-
             $select : [
-
-              new ResultColumn(new ColumnExpression('code_master', 'codeType'), 'codeType'),
-              new ResultColumn(new ColumnExpression('code_master', 'code'), 'code'),
-              new ResultColumn(new FunctionExpression('MAX', new ColumnExpression('code_master', 'partyGroupCode')), 'partyGroupCode')
-
+              new ResultColumn(new ColumnExpression('calc_code_master', 'codeType'), 'codeType'),
+              new ResultColumn(new ColumnExpression('calc_code_master', 'code'), 'code'),
+              new ResultColumn(new FunctionExpression('MAX', new ColumnExpression('calc_code_master', 'partyGroupCode')), 'partyGroupCode')
             ],
-
+            $from: new FromTable('code_master', 'calc_code_master'),
             // warning !!! : deletedBy must be Null!!!!!!
             // warning !!! : deletedBy must be Null!!!!!!
             // warning !!! : deletedBy must be Null!!!!!!
@@ -46,105 +38,118 @@ const query = new QueryDef(
             // warning !!! : deletedBy must be Null!!!!!!
             // warning !!! : deletedBy must be Null!!!!!!
             $where: [
-              new IsNullExpression(new ColumnExpression('code_master', 'deletedBy'), false),
-              new IsNullExpression(new ColumnExpression('code_master', 'deletedAt'), false)
+              new IsNullExpression(new ColumnExpression('calc_code_master', 'deletedBy'), false),
+              new IsNullExpression(new ColumnExpression('calc_code_master', 'deletedAt'), false)
             ],
-
-            $from: new FromTable('code_master'),
             $group : new GroupBy([
-              'codeType', 'code'
+              new ColumnExpression('calc_code_master', 'codeType'),
+              new ColumnExpression('calc_code_master', 'code')
             ]),
-
           }),
           $as : 'leftTable',
           joinClauses : [
             new JoinClause(
               'LEFT',
-              new FromTable('code_master'),
-
+              new FromTable('code_master', 'right_code_master'),
               new AndExpressions([
-
                 // code is same
                 new BinaryExpression(
                   new BinaryExpression(
                     new ColumnExpression('leftTable', 'code'),
                     '=',
-                    new ColumnExpression('code_master', 'code')
+                    new ColumnExpression('right_code_master', 'code')
                   )
                 ),
-
                 // codeType is same
-
                 new OrExpressions([
-
                   // codeType is not null case
                   new AndExpressions([
-
-                    new IsNullExpression(new ColumnExpression('leftTable', 'codeType'), true),
-
+                    new IsNullExpression(
+                      new ColumnExpression('leftTable', 'codeType'),
+                      true
+                    ),
                     new BinaryExpression(
                       new BinaryExpression(
                         new ColumnExpression('leftTable', 'codeType'),
                         '=',
-                        new ColumnExpression('code_master', 'codeType')
+                        new ColumnExpression('right_code_master', 'codeType')
                       )
                     )
-
                   ]),
-
                   // codeType is null case
                   new AndExpressions([
-
-                    new IsNullExpression(new ColumnExpression('leftTable', 'codeType'), false),
-                    new IsNullExpression(new ColumnExpression('code_master', 'codeType'), false)
-
+                    new IsNullExpression(
+                      new ColumnExpression('leftTable', 'codeType'),
+                      false
+                    ),
+                    new IsNullExpression(
+                      new ColumnExpression('right_code_master', 'codeType'),
+                      false
+                    )
                   ]),
-
                 ]),
-
                 // partyGroupCode is same
-
                 new OrExpressions([
-
                   // partyGroupCode is not null case
                   new AndExpressions([
-
-                    new IsNullExpression(new ColumnExpression('leftTable', 'partyGroupCode'), true),
-
+                    new IsNullExpression(
+                      new ColumnExpression('leftTable', 'partyGroupCode'),
+                      true
+                    ),
                     new BinaryExpression(
                       new BinaryExpression(
                         new ColumnExpression('leftTable', 'partyGroupCode'),
                         '=',
-                        new ColumnExpression('code_master', 'partyGroupCode')
+                        new ColumnExpression('right_code_master', 'partyGroupCode')
                       )
                     )
-
                   ]),
-
                   // partyGroupCode is null case
                   new AndExpressions([
-
-                    new IsNullExpression(new ColumnExpression('leftTable', 'partyGroupCode'), false),
-                    new IsNullExpression(new ColumnExpression('code_master', 'partyGroupCode'), false)
-
+                    new IsNullExpression(
+                      new ColumnExpression('leftTable', 'partyGroupCode'),
+                      false
+                    ),
+                    new IsNullExpression(
+                      new ColumnExpression('right_code_master', 'partyGroupCode'),
+                      false
+                    )
                   ]),
-
                 ])
-
               ])
-
             )
           ]
         })
       })
-
     })
-
   })
 )
 
-query.register('isActive',
-{
+query.register('flex_data_join', new Query({
+  $from: new FromTable({
+    table: 'code_master',
+    joinClauses : [
+      new JoinClause({
+        operator: 'LEFT',
+        table: 'flex_data',
+        $on: [
+          new BinaryExpression(
+            new ColumnExpression('flex_data', 'tableName'),
+            '=',
+            'code_master'
+          ),
+          new BinaryExpression(
+            new ColumnExpression('code_master', 'id'),
+            '=',
+            new ColumnExpression('flex_data', 'primaryKey')
+          ),
+        ],
+      })
+    ],
+  })
+}))
+
+query.register('isActive', {
   expression : new FunctionExpression(
     'IF',
     new AndExpressions([
