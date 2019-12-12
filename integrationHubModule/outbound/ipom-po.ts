8a6f64f3d455b94b3c82090a4c272d1d21a0153a
Ken Chan <ken.chan@swivelsoftware.com>
2019-12-12 09:58:10 +0800

    fix tracking bug

--- integrationHubModule/outbound/ipom-po.ts
+++ integrationHubModule/outbound/ipom-po.ts
@@ -1,15 +1,17 @@
 export default {
   method: 'POST', // 'GET'|'POST'|'PUT'|'DELETE'|'HEAD'|'OPTIONS'
   getUrl: (headers: any, body: any) => {
-    return 'http://ecxipomapi_uat.swivelsoftware.com/ipomapi.asmx'
+    return 'https://ecxdillardsapi.swivelsoftware.asia/po360.ashx'
   },
   requestHandler: (headers: any, body: any) => {
+    const { errors, ...rest } = body.pojson
+    console.log({ pojson: rest }, 'SEND EDI')
     return {
       headers: {
         ...headers,
         'Content-Type': 'application/json',
       },
-      data: body,
+      body: JSON.stringify({ pojson: rest }),
     }
   },
   responseHandler: (response: { responseBody: any; responseOptions: any }) => {
