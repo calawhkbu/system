547e669faf3e4280b33a888d7d8e2eccccd03952
Kennys Ng <kennys.ng@swivelsoftware.com>
2020-05-06 17:30:38 +0800

    move getDefaultParams to backend custom

---
+++ sequelizeModule/base.ts
@@ -0,0 +1,56 @@
+import { IConditionalExpression, OrExpressions, AndExpressions } from 'node-jql'
+import { JwtPayload } from 'modules/auth/interfaces/jwt-payload'
+import { Transaction } from 'sequelize'
+import { Role } from 'models/main/role'
+
+export default async function getDefaultParams(
+  conditions?: IConditionalExpression,
+  queryName?: string,
+  user?: JwtPayload,
+  transaction?: Transaction
+): Promise<IConditionalExpression> {
+  if (user) {
+    const roleService = this.helper.roleService || this
+    const roles =
+      user.selectedRoles && user.selectedRoles.length
+        ? await (roleService as any).find({
+            where: {
+              id: { $in: user.selectedRoles.map(role => role.id) },
+            },
+            transaction,
+          })
+        : []
+    if (roles.length) {
+      const rolesExpressions = roles
+        .reduce((all: { group: string; roles: any[] }[], result: Role) => {
+          const sameGroupIndex = all.findIndex(t => t.group === result.roleGroup) // is same group
+          if (result && result.filter && result.filter[this.repository.getTableName()]) {
+            const { jql } = result.filter[this.repository.getTableName()]
+            if (jql) {
+              if (sameGroupIndex > -1) {
+                const roles = all[sameGroupIndex].roles
+                roles.push(jql)
+                all.splice(sameGroupIndex, 1, {
+                  group: result.roleGroup,
+                  roles,
+                })
+              } else {
+                all.push({ group: result.roleGroup, roles: [jql] })
+              }
+            }
+          }
+          return all
+        }, [])
+        .map((r: any) => new OrExpressions({ expressions: r.roles }))
+      if (rolesExpressions && rolesExpressions.length) {
+        conditions = conditions
+          ? new AndExpressions({
+              expressions: [conditions, ...rolesExpressions],
+            })
+          : rolesExpressions
+      }
+    }
+  }
+
+  return conditions
+}
