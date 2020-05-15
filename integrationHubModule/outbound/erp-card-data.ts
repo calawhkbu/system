9bd0885b740dd800493df83809f5c15b93391ff1
Kennys Ng <kennys.ng@swivelsoftware.com>
2020-05-15 18:00:44 +0800

    variable -> constants

--- integrationHubModule/outbound/erp-card-data.ts
+++ integrationHubModule/outbound/erp-card-data.ts
@@ -1,8 +1,8 @@
 import { NotImplementedException, NotFoundException, BadRequestException } from '@nestjs/common'
 import axios from 'axios'
 
-const app = {
-  variable: {
+export default {
+  constants: {
     getPostProcessFunc: null as Function,
     partyGroup: null as any,
     card: null as any,
@@ -10,27 +10,27 @@ const app = {
     zyh: 0,
   },
   method: 'GET',
-  getUrl: async({ id, partyGroup: { api } }: any, params: any, constants: { [key: string]: any }, helper: { [key: string]: Function }): Promise<string> => {
+  getUrl: async({ id, partyGroup: { api } }: any, params: any, constants: any, helper: { [key: string]: Function }): Promise<string> => {
     if (!api.erp || !api.erp.url) throw new NotImplementedException('ERP_NOT_LINKED')
     if (!params.subqueries || !params.subqueries.type) throw new BadRequestException('MISSING_TYPE')
-    const card = app.variable.card = await helper.getCard({
+    const card = constants.card = await helper.getCard({
       method: 'POST',
       headers: {
         'content-type': 'application/json',
       },
       url: `${api.erp.url}/getschrptdata`,
       data: {
-        zyh: app.variable.zyh = id,
+        zyh: constants.zyh = id,
         zyd: params.subqueries.type.value,
       },
     })
     console.log(card.dlink, 'erp-card-data:system')
     return card.dlink
   },
-  requestHandler: ({ getPostProcessFunc, partyGroup, user }: any): any => {
-    app.variable.getPostProcessFunc = getPostProcessFunc
-    app.variable.partyGroup = partyGroup
-    app.variable.user = user
+  requestHandler: ({ getPostProcessFunc, partyGroup, user }: any, params: any, constants: any): any => {
+    constants.getPostProcessFunc = getPostProcessFunc
+    constants.partyGroup = partyGroup
+    constants.user = user
     return {
       headers: {
         'content-type': 'application/json',
@@ -39,10 +39,10 @@ const app = {
   },
   responseHandler: async(
     response: { responseBody: any; responseOptions: any },
-    constants: { [key: string]: any },
+    constants: any,
     helper: { [key: string]: Function }
   ) => {
-    const { card, getPostProcessFunc, partyGroup, user, zyh } = app.variable
+    const { card, getPostProcessFunc, partyGroup, user, zyh } = constants
 
     let responseBody = helper.parseData(response.responseBody, card)
 
@@ -52,5 +52,3 @@ const app = {
     return { ...response, responseBody }
   },
 }
-
-export default app
