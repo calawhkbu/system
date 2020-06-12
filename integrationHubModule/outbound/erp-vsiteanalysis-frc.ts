d0730dbfadb116facd7285ceae052afdad7f2124
Kennys Ng <kennys.ng@swivelsoftware.com>
2020-06-12 15:34:59 +0800

    revert commit

--- integrationHubModule/outbound/erp-vsiteanalysis-frc.ts
+++ integrationHubModule/outbound/erp-vsiteanalysis-frc.ts
@@ -143,7 +143,8 @@ const app = {
     // xgrpname
     // warning : getting the first one only
     let xgrpname = ''
-    if (subqueries.agentGroup && subqueries.agentGroup.length) xgrpname = JSON.stringify(subqueries.agentGroup.value)
+    if (subqueries.agentGroup) xgrpname = subqueries.agentGroup.value[0]
+    // TODO if (subqueries.agentGroup && subqueries.agentGroup.length) xgrpname = JSON.stringify(subqueries.agentGroup.value)
 
     // inblno && exblno
     const xHouseNo = {
