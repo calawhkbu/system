99cf7397afae3fe9bc9de152dd02efda46e1cc93
Kennys Ng <kennys.ng@swivelsoftware.com>
2020-08-26 10:14:33 +0800

    support iPOM cards

---
+++ integrationHubModule/outbound/ipom-cards.ts
@@ -0,0 +1,27 @@
+import { NotImplementedException } from '@nestjs/common'
+
+export default {
+  method: 'POST',
+  getUrl: ({ api }: { api: any }) => {
+    if (!api.ipom || !api.ipom.url) throw new NotImplementedException()
+    return `${api.ipom.url}/getschrptlist`
+  },
+  requestHandler: ({ api }: { api: any }) => {
+    const result = {
+      headers: {
+        'content-type': 'application/json',
+      },
+    } as any
+    if (api.ipom.body) result.body = JSON.stringify(api.ipom.body)
+    return result
+  },
+  responseHandler: (response: { responseBody: any; responseOptions: any }, constants: { [key: string]: any }, helper: { [key: string]: Function }) => {
+    // parse results
+    let responseBody = JSON.parse(JSON.parse(response.responseBody).d) as any[]
+
+    // reformat
+    responseBody = helper.parseCards(responseBody, 'ipom', 'Swivel POM')
+
+    return { ...response, responseBody }
+  },
+}
